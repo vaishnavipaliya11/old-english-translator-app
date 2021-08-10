e1134d5f86d7eec152c4e9edbee6a9d8598f498a
vaishnavi paliya <81965405+vaishnavipaliya11@users.noreply.github.com>
2021-08-10 15:10:02 +0530

    url replaced and error handled

--- app.js
+++ app.js
@@ -1,19 +1,46 @@
 var btnTranslate = document.querySelector("#btn-translate")
 var input = document.querySelector("#txt-input")
 var outputDiv =  document.querySelector(".output")
-
-var url ="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
+var errorDiv = document.querySelector("#error")
+console.log(errorDiv);
+var url ="https://api.funtranslations.com/translate/oldenglish.json"
 
 function setUrl(text){
   return url +"?"+"text="+text
 }
 
 function clickHandler(userInput) {
+
   var userInput = input.value
 
-fetch(setUrl(userInput))
-.then(response=> response.json())
-.then(json => console.log(json)) 
+  if(userInput==""){
+  emptyTextError()
+
+}else{
+
+errorDiv.style.display="none"
+
+  fetch(setUrl(userInput))
+  .then(response=> response.json())
+  .then(json => {
+    var translatedText= json.contents.translated
+    outputDiv.innerText=translatedText
+  }).catch(errorHandler)
+  }
+
+  function errorHandler(error){
+    alert("try after some time")
+  }
 }
+
+function emptyTextError(){
+  errorDiv.style.display="block";
+  outputDiv.style.display="none";
+  errorDiv.innerHTML="you have not given the text . Please refresh the page and enter the text !!"
+}
+
+
+
+
 
 btnTranslate.addEventListener("click",clickHandler)
