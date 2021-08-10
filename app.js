82fdc4d8c3f3b2c95b2bc78c87fcba7ef8ec39f2
vaishnavi paliya <81965405+vaishnavipaliya11@users.noreply.github.com>
2021-08-10 13:58:02 +0530

    wired input and output

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var outputDiv =  document.querySelector(".output")
 
 function clickHandler() {
   console.log("clicked");
+  var userInput = input.value;
+  console.log(userInput)
+  outputDiv.innerText= "hello"
 }
 
 btnTranslate.addEventListener("click",clickHandler)
