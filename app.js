6aab085667f8c776c219cb760fd7e8d01c5550df
vaishnavi754 <81965405+vaishnavipaliya11@users.noreply.github.com>
2022-01-11 21:23:57 -0800

    refactor: functions and variables refactored

--- app.js
+++ app.js
@@ -1,46 +1,39 @@
-var btnTranslate = document.querySelector("#btn-translate")
-var input = document.querySelector("#txt-input")
-var outputDiv =  document.querySelector(".output")
-var errorDiv = document.querySelector("#error")
-console.log(errorDiv);
-var url ="https://api.funtranslations.com/translate/oldenglish.json"
+const btnTranslate = document.querySelector("#btn-translate")
+const input = document.querySelector("#txt-input")
+const outputDiv =  document.querySelector(".output")
+const errorDiv = document.querySelector("#error")
 
-function setUrl(text){
-  return url +"?"+"text="+text
-}
+const url ="https://api.funtranslations.com/translate/oldenglish.json"
 
-function clickHandler(userInput) {
 
+const setUrl = (text)=> url+"?"+"text="+text 
+
+const clickHandler=(userInput)=> {
   var userInput = input.value
-
   if(userInput==""){
   emptyTextError()
+}
 
-}else{
+else
+{
+let errorHandler =(error)=> {alert ("try after some time")}
 
 errorDiv.style.display="none"
 
   fetch(setUrl(userInput))
   .then(response=> response.json())
   .then(json => {
-    var translatedText= json.contents.translated
+    const translatedText= json.contents.translated
     outputDiv.innerText=translatedText
   }).catch(errorHandler)
   }
 
-  function errorHandler(error){
-    alert("try after some time")
-  }
 }
 
-function emptyTextError(){
+const emptyTextError = ()=>{
   errorDiv.style.display="block";
   outputDiv.style.display="none";
   errorDiv.innerHTML="you have not given the text . Please refresh the page and enter the text !!"
 }
-
-
-
-
 
 btnTranslate.addEventListener("click",clickHandler)
